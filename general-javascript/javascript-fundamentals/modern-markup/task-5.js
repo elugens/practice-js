cf1262b672ecf015f654323b6ce52e2016f639cd
Stacey Wilson <elugens@gmail.com>
2024-01-16 19:34:54 +0000

    update

--- general-javascript/javascript-fundamentals/modern-markup/task-5.js
+++ general-javascript/javascript-fundamentals/modern-markup/task-5.js
@@ -1,5 +1,25 @@
 // Question: What is async/await in JavaScript?
-// Link: https://www.hellojavascript.info/docs/general-javascript-questions/javascript-fundamentals/modern-markup#what-is-a-promise-in-javascript
+// Link: https://www.hellojavascript.info/docs/general-javascript-questions/javascript-fundamentals/modern-markup#what-is-asyncawait-in-javascript
 // Task: Create function called main that calls another function asynchronously usit the asnyc await syntax
 
 ///// Solution Example /////
+
+function fetchData(name) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const data = `Hello, ${name}!`;
+        resolve(data);
+      }, 2000);
+    });
+  }
+  
+  async function main() {
+    try {
+      const response = await fetchData('JavaScript');
+      console.log(response); // Output: Hello, JavaScript!
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  main();
